172a9ded09e9ca2e0ae4eb843075959e93ae65f5
ahmedturk15943 <ahmedturk748972@gmail.com>
2024-12-17 14:45:18 +0500

    Add files via upload

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,14 @@
+// src/app/blog/layout.tsx
+
+import React from 'react';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      {/* You can add your layout components or structure here */}
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+//import Image from "next/image";//
+import HomePage from './HomePage/page'
+import React from 'react'
+// import Menu from "./Manu/page";
+
+
+
+
+export default function Home() {
+  return (
+    <div className="mt-5">
+          <HomePage />
+    </div>
+  );
+}
