2e96e0d4a938d725cf2a1beebfa433a39dd968e3
AHMED RAZA TURK <ahmedturk8736@gmail.com>
2025-02-01 21:58:35 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,43 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["cdn.sanity.io"],
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
-
-
-
-
-
-
-
-
-import { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
-  experimental: {
-    serverActions: true, // Agar Next.js 15 use kar rahe hain toh ye add karna zaroori hai
-  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
+
+
+
+
+// import { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ["cdn.sanity.io"],
+//   },
+//   experimental: {
+//     serverActions: true, // Agar Next.js 15 use kar rahe hain toh ye add karna zaroori hai
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
+
+
+// import { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ["cdn.sanity.io"],
+//   },
+//   experimental: {
+//     // serverActions: true, // Is line ko hata dein ya comment kar dein
+//   },
+// };
+
+// export default nextConfig;
