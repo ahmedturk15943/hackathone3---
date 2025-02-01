c42e68fdeb26125d6428de0ae772644e875ec55b
AHMED RAZA TURK <ahmedturk8736@gmail.com>
2025-02-01 19:00:01 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["cdn.sanity.io"],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
+
+
+
+
+
+
+
+
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  experimental: {
+    serverActions: true, // Agar Next.js 15 use kar rahe hain toh ye add karna zaroori hai
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
