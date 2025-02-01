6442d47e05b32411a72a8103fbf8400a8dff7237
AHMED RAZA TURK <ahmedturk8736@gmail.com>
2025-02-01 18:49:10 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,34 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// };
+
+
+
+
+
+
+
+import { createMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export default createMiddleware({
+  beforeAuth(req) {
+    return NextResponse.next();
+  },
+  afterAuth(auth, req) {
+    return NextResponse.next();
+  },
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!_next|.*\\..*).*)"], // Static files ko ignore karne ke liye
 };
